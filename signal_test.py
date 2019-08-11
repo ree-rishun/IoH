1e6123ea3e746a7e71f982c621ac4a52da656fbc
reerishun <ree0432@gmail.com>
2019-08-12 05:24:56 +0900

    add signal_test.py

---
+++ signal_test.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# import
+import time
+import sys
+import wiringpi
+import RPi.GPIO as GPIO
+
+# 宣言
+    # GPIO Pin
+SPICLK = 11
+SPIMOSI = 10
+SPIMISO = 9
+SPICS = 8
+
+# GPIO config
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICS, GPIO.OUT)
+
+# ADC関数
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+    if adcnum > 7 or adcnum < 0:
+        return -1
+    GPIO.output(cspin, GPIO.HIGH)
+    GPIO.output(clockpin, GPIO.LOW)
+    GPIO.output(cspin, GPIO.LOW)
+
+    commandout = adcnum
+    commandout |= 0x18  # スタートビット＋シングルエンドビット
+    commandout <<= 3    # LSBから8ビット目を送信するようにする
+    for i in range(5):
+        # LSBから数えて8ビット目から4ビット目までを送信
+        if commandout & 0x80:
+            GPIO.output(mosipin, GPIO.HIGH)
+        else:
+            GPIO.output(mosipin, GPIO.LOW)
+        commandout <<= 1
+        GPIO.output(clockpin, GPIO.HIGH)
+        GPIO.output(clockpin, GPIO.LOW)
+    adcout = 0
+    # 13ビット読む（ヌルビット＋12ビットデータ）
+    for i in range(13):
+        GPIO.output(clockpin, GPIO.HIGH)
+        GPIO.output(clockpin, GPIO.LOW)
+        adcout <<= 1
+        if i>0 and GPIO.input(misopin)==GPIO.HIGH:
+            adcout |= 0x1
+    GPIO.output(cspin, GPIO.HIGH)
+    return adcout
+    
+# main loop
+while True:
+    try:
+        # ADCから値を取得
+        inputVal = readadc(0, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        print(inputVal)
+
+    except (KeyboardInterrupt, SystemExit):
+        exit()
